ad1def20fad6469000f234dfa68a971e24f67f83
lubyshev <demagog@demagog.demagog>
2020-08-11 19:34:56 +0300

    DotEnv

--- config/db.php
+++ config/db.php
@@ -1,14 +1,13 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+$host   = getenv('DB_HOST');
+$port   = (int)getenv('DB_PORT');
+$dbName = getenv('DB_NAME');
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+return [
+    'class'    => 'yii\db\Connection',
+    'dsn'      => "mysql:host={$host};port={$port};dbname={$dbName}",
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'charset'  => 'utf8',
 ];
