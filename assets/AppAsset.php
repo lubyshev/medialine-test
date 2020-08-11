6453a87b075756aae3b1354ba825e905c0356ae3
lubyshev <demagog@demagog.demagog>
2020-08-12 02:22:06 +0300

    Pretty url + vue.js

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -23,6 +23,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
