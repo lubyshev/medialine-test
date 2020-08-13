4346ef446a9fd4a7d1d162f040f97bf66e16eb04
lubyshev <demagog@demagog.demagog>
2020-08-14 01:47:46 +0300

    Remove unused elements

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,14 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace app\controllers;
 
-use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
 
 class SiteController extends Controller
 {
@@ -20,10 +15,6 @@ class SiteController extends Controller
         return [
             'error'   => [
                 'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class'           => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
         ];
     }
